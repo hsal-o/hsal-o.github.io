98348797d0ab2ff41a74f34f04b8fc7a6245dd36
Yumba <hmsalinas31@outlook.com>
2024-06-22 18:50:40 -0500

    Updated Projects Page
    - Added repo language and language color to each repo.
    - Removed emojis from Projects and Education pages.

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -1,7 +1,12 @@
 async function fetchRepos() {
     const username = 'hsal-o';
-    const response = await fetch(`https://api.github.com/users/${username}/repos`);
-    const repos = await response.json();
+    const [reposResponse, colorsResponse] = await Promise.all([
+        fetch(`https://api.github.com/users/${username}/repos`),
+        fetch('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json')
+    ]);
+
+    const repos = await reposResponse.json();
+    const colors = await colorsResponse.json();
 
     const reposContainer = document.getElementById('repos-box');
     repos.forEach(repo => {
@@ -11,13 +16,34 @@ async function fetchRepos() {
             window.open(repo.html_url, '_blank');
         };
 
+        // Create repo content
+        const repoContent = document.createElement('div');
+
         const repoTitle = document.createElement('h2');
-        repoTitle.textContent = repo.name;
-        repoDiv.appendChild(repoTitle);
+        repoTitle.textContent = repo.name;  
+        repoContent.appendChild(repoTitle);
 
         const repoDescription = document.createElement('p');
         repoDescription.textContent = repo.description || 'No description provided';
-        repoDiv.appendChild(repoDescription);
+        repoContent.appendChild(repoDescription);
+
+        // Create repo footer
+        const repoFooter = document.createElement('div');
+
+        const repoLanguage = document.createElement('span');
+        repoLanguage.textContent = repo.language;
+        repoLanguage.className = 'language'
+
+        const languageColor = document.createElement('span');
+        languageColor.className = 'language-color';
+        languageColor.style.backgroundColor = colors[repo.language]?.color || '#000';
+        repoLanguage.prepend(languageColor);
+
+        repoFooter.appendChild(repoLanguage);
+
+        // Add repo content + footer to main div container
+        repoDiv.appendChild(repoContent)
+        repoDiv.appendChild(repoFooter)
 
         reposContainer.appendChild(repoDiv);
     });
